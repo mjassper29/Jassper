ee811646edae30845069046f06fc169681da1e15
mjassper29 <mjassper29@yahoo.com>
2022-06-06 20:04:37 -0700

    first commit

---
+++ console.js
@@ -0,0 +1,23 @@
+// performing a task
+function greet(name, lastName) {
+    console.log('hello '+ name +''+ lastName)
+}
+greet('Jassper',' Alipao');
+
+// calculates a value
+function square(number) {
+    return number * number;
+}
+
+let number = square(2);
+
+
+var ourName = 'freeCodeCamp';
+var ourStr = 'Hello, our name is ' + ourName + ', How are you?';
+
+var myName = 'Jassper';
+var myStr = ' My name is ' + myName + ' and I am well!';
+ourStr += myStr;
+
+ourName = ourName.length
+ 
